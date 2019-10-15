f6b0d25a99431144a4372ace467bfa2608c33d16
MarietaKocharian <98marikoch@gmail.com>
2019-10-15 17:10:45 +0400

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./Component/creatTodo";
+import EditTodo from "./Component/editTodo";
+import TodoList from "./Component/todoList";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
         <Router>
-          <div className="container">
-            <h2>MERNS-stack ToDo App</h2>
-          </div>
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                    </a>
+                    <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+                    <div className="collpase navbar-collapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="navbar-item">
+                                <Link to="/" className="nav-link">Todos</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/create" className="nav-link">Create Todo</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <br/>
+                <Route path="/" exact component={TodoList}/>
+                <Route path="/edit/:id" component={EditTodo}/>
+                <Route path="/create" component={CreateTodo}/>
+            </div>
         </Router>
-
     );
   }
 }
