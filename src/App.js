1a5c2c3d52a8ea01e2dfdeb18c6e302047f4a2f1
MarietaKcocharyan <98marikoch@gmail.com>
2019-10-26 21:15:51 +0400

    Finish project

--- src/App.js
+++ src/App.js
@@ -2,38 +2,40 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateTodo from "./Component/creatTodo";
-import EditTodo from "./Component/editTodo";
-import TodoList from "./Component/todoList";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
-import './App.css';
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
-        <Router>
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-                    </a>
-                    <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-                    <div className="collpase navbar-collapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="navbar-item">
-                                <Link to="/" className="nav-link">Todos</Link>
-                            </li>
-                            <li className="navbar-item">
-                                <Link to="/create" className="nav-link">Create Todo</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                <br/>
-                <Route path="/" exact component={TodoList}/>
-                <Route path="/edit/:id" component={EditTodo}/>
-                <Route path="/create" component={CreateTodo}/>
+      <Router>
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
             </div>
-        </Router>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
